b641c78b72cf9d42485ae4f6de0fb60bf7036f2f
dag7dev <44711271+dag7dev@users.noreply.github.com>
2021-06-21 17:28:43 +0200

    refactor: code has been updated in order to be compliant with JSQuizee engine

---
+++ js/config.js
@@ -0,0 +1,22 @@
+////////////
+// CONFIG //
+////////////
+var nMinutes = 25; // 25, suggested
+var rightAnswerPoints = 2
+var wrongAnswerPoints = 1
+var noAnswerPoints = 0
+var numberOfQuestions = 40 // default number of questions that you want to show
+
+// CUSTOMIZABLE PART OF THE PAGE - SHOWS OR HIDES SOME PARTS
+// if a part is disabled, then the related functionality will be disabled
+var showSlider = true
+var showTimer = true
+var showShuffleQuestions = true
+var showShuffleAnswers = true
+
+// FILES
+var jsonFolder = "json" // json folder where to load your json files
+var jsonFiles = { // load other quizzes
+    "questions_1.json": "SO1",
+    "questions_2.json": "SO2",
+};
