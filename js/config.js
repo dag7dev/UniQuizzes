1e9715c951d23b9c2e831d8bb9e5295828eb0e84
dag7dev <44711271+dag7dev@users.noreply.github.com>
2021-06-24 21:23:07 +0200

    stashed changes from SO1_experimental

--- js/config.js
+++ js/config.js
@@ -18,6 +18,5 @@ var showShuffleAnswers = true
 var jsonFolder = "json" // json folder where to load your json files
 var jsonFiles = { // load other quizzes
     "questions_1.json": "SO1",
-    "questions_2.json": "SO2",
-    "so1_untested.json": "SO1 Untested"
+    "questions_2.json": "SO2"
 };
