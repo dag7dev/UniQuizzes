aeccaa52f1250e223ddfc487c0ac529734f7883e
dag7dev <dag7@protonmail.com>
2022-02-01 19:21:38 +0100

    enh: title is now customizable

--- js/config.js
+++ js/config.js
@@ -6,8 +6,9 @@ var rightAnswerPoints = 2
 var wrongAnswerPoints = 1
 var noAnswerPoints = 0
 var numberOfQuestions = 40 // default number of questions that you want to show
+var title = "JSQuizee"
 
-// CUSTOMIZABLE PART OF THE PAGE - SHOWS OR HIDES SOME PARTS
+// CUSTOMIZABLE PARTS OF THE PAGE - SHOWS OR HIDES SOME PARTS
 // if a part is disabled, then the related functionality will be disabled
 var showSlider = true
 var showTimer = true
